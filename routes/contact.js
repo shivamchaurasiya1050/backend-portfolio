79d54284e9399c290d9becfecf41d3fbf1683992
shivam chaurasiya <shivamchaurasiya1050@gmail.com>
2026-02-26 17:40:04 +0530

    fresh clean commit

--- routes/contact.js
+++ routes/contact.js
@@ -18,6 +18,9 @@ router.post("/", async (req, res) => {
     // Create transporter
     const transporter = nodemailer.createTransport({
       service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
       auth: {
         user: "shivamatlas15@gmail.com",
         pass: "fjzf pjgq qvpf rzcu",
@@ -27,7 +30,7 @@ router.post("/", async (req, res) => {
     // Email options
     const mailOptions = {
       from: email,
-      to: 'shivamchaurasiya1050@gmail.com',
+      to: "shivamchaurasiya1050@gmail.com",
       subject: `New Contact Message: ${subject}`,
       html: `
         <h3>New Contact Form Submission</h3>
@@ -40,8 +43,9 @@ router.post("/", async (req, res) => {
 
     await transporter.sendMail(mailOptions);
 
-    res.status(201).json({ message: "Message sent successfully and email delivered ✅" });
-
+    res
+      .status(201)
+      .json({ message: "Message sent successfully and email delivered ✅" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Server Error ❌" });
